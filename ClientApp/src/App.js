4fb14332abf6a5141c01ccb6c842b321756b6910
Thiago Caramelo <caramelot@knowfully.com>
2021-07-15 19:02:35 -0300

    Specifying Attributes

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 
 export default function App() {
   const singer = "Jimmy Cliff";
-  const isLoading = true;
+  const photo_width = 100;
 
-  if (isLoading) return <h2>...Loading</h2>;
+  const photo = (
+    <img
+      src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRONhv9WOZOXCOKHhgoObhmN6Ieh0hshF586ierCLwjZQhezDuB"
+      width={photo_width}
+    />
+  );
 
   const element = (
     <>
       <h1>♫ I can see clearly now the rain is gone by {singer}♫</h1>
+      {photo}
     </>
   );
   return element;
