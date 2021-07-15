741e71408041538f3dd185248a2803e9da636b7a
Thiago Caramelo <caramelot@knowfully.com>
2021-07-15 17:10:01 -0300

    clean up

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,18 +1,5 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import React from "react";
 
-export class Layout extends Component {
-  static displayName = Layout.name;
-
-  render () {
-    return (
-      <div>
-        <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
+export function Layout(props) {
+  return <div>{props.children}</div>;
 }
