5b839edfbcaf2ed00a5ba8222ffe635a2127ae4e
Thiago Caramelo <caramelot@knowfully.com>
2021-07-15 18:38:35 -0300

    add more examples

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,8 +2,24 @@ import React from "react";
 
 export default function App() {
   const singer = "Jimmy Cliff";
+  const albums = ["Hard Road", "Another Cycle", "Goodbye Yesterday"];
+
+  function formatName(singer = "") {
+    return singer.toUpperCase();
+  }
+
   const element = (
-    <h1>♫ I can see clearly now the rain is gone by {singer}♫</h1>
+    <>
+      <h1>♫ I can see clearly now the rain is gone by {formatName(singer)}♫</h1>
+      <h2>Albums</h2>
+      <ul>
+        {albums.map((album) => (
+          <li>
+            <h2>{album}</h2>
+          </li>
+        ))}
+      </ul>
+    </>
   );
   return element;
 }
