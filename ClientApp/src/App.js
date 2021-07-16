6d60f3d1074996e305f2af0a2828811503c2f7b8
Thiago Caramelo <caramelot@knowfully.com>
2021-07-16 16:26:44 -0300

    create photo component

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
+import Photo from "./components/Photo";
 
 export default function App() {
   const singer = "Jimmy Cliff";
   const photo_width = 100;
 
-  const photo = (
-    <img
-      src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRONhv9WOZOXCOKHhgoObhmN6Ieh0hshF586ierCLwjZQhezDuB"
-      width={photo_width}
-    />
-  );
-
   const element = (
     <>
       <h1>♫ I can see clearly now the rain is gone♫</h1>
       <h3>{singer}</h3>
-      {photo}
+      <Photo
+        src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRONhv9WOZOXCOKHhgoObhmN6Ieh0hshF586ierCLwjZQhezDuB"
+        photo_width={photo_width}
+      />
     </>
   );
   return element;
