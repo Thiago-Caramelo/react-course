728b1aaddf8cb9ddb542911aa509f6fa32d0151d
Thiago Caramelo <caramelot@knowfully.com>
2021-07-17 11:07:03 -0300

    Conditional rendering

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -7,19 +7,22 @@ export default function App() {
 
   if (isLoading) return <h2>...Loading</h2>;
 
-  // const component = "componentName";
-
-  // switch (component) {
-  //   case "componentName":
-  //     return <div>Test</div>;
-  //   default:
-  //     return <div>Default</div>;
-  // }
+  let elementVariable;
+  switch (singer) {
+    case "Jimmy Cliff":
+      elementVariable = <div>:)</div>;
+      break;
+    default:
+      elementVariable = <div>:(</div>;
+      break;
+  }
 
   const element = (
     <>
       <h1>♫ I can see clearly now the rain is gone♫</h1>
-      <h3>{showSinger && singer}</h3>
+      <h3>
+        {showSinger && singer} {elementVariable}
+      </h3>
     </>
   );
   return element;
