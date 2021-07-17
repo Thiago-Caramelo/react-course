3a6137399aff5fe0218aede52c8e63b574ad2340
Thiago Caramelo <caramelot@knowfully.com>
2021-07-17 11:00:23 -0300

    Conditional rendering

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,14 +2,24 @@ import React from "react";
 
 export default function App() {
   const singer = "Jimmy Cliff";
+  const showSinger = true;
   const isLoading = true;
 
   if (isLoading) return <h2>...Loading</h2>;
 
+  // const component = "componentName";
+
+  // switch (component) {
+  //   case "componentName":
+  //     return <div>Test</div>;
+  //   default:
+  //     return <div>Default</div>;
+  // }
+
   const element = (
     <>
       <h1>♫ I can see clearly now the rain is gone♫</h1>
-      <h3>{singer}</h3>
+      <h3>{showSinger && singer}</h3>
     </>
   );
   return element;
