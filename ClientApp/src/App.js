3590977950f114e65a9cebecf90870aeede051c3
Thiago Caramelo <caramelot@knowfully.com>
2021-07-20 20:54:26 -0300

    useMemo

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,11 +1,19 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useCallback } from "react";
 import Layout from "./components/Layout";
 import Footer from "./components/Footer";
 import Expensive from "./components/Expensive";
 
 export default function App() {
   const list = ["a", "b", "c"];
-  const expensive = useMemo(() => <Expensive list={list} />, [list]);
+
+  const onSubmit = useCallback((parameters) => {
+    console.log(parameters);
+  }, []);
+
+  const expensive = useMemo(
+    () => <Expensive list={list} onSubmit={onSubmit} />,
+    [list, onSubmit]
+  );
   return (
     <>
       <Layout footer={<Footer />}>
--- ClientApp/src/components/Expensive.js
+++ ClientApp/src/components/Expensive.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function Expensive(props) {
+  props.onSubmit({ date: new Date() });
   return (
     <ul>
-      {props.list.map((item) => {
-        return <li>{item}</li>;
+      {props.list.map((item, index) => {
+        return <li key={index}>{item}</li>;
       })}
     </ul>
   );
